9b5010782c8dc37e3df45e4c676fbcafb4f1f8ff
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2017-10-05 10:55:17 +0200

    Generate test class for MyPoint

---
+++ test/fr/ut2j/m1ice/ootesting/MyPointTest.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package test.fr.ut2j.m1ice.ootesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Kaze
+ *
+ */
+public class MyPointTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#MyPoint()}.
+	 */
+	@Test
+	public void testMyPoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#MyPoint(double, double)}.
+	 */
+	@Test
+	public void testMyPointDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#MyPoint(main.fr.ut2j.m1ice.ootesting.MyPoint)}.
+	 */
+	@Test
+	public void testMyPointMyPoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#setX(double)}.
+	 */
+	@Test
+	public void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#setY(double)}.
+	 */
+	@Test
+	public void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#getX()}.
+	 */
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#getY()}.
+	 */
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#scale(double)}.
+	 */
+	@Test
+	public void testScale() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#horizontalSymmetry(main.fr.ut2j.m1ice.ootesting.MyPoint)}.
+	 */
+	@Test
+	public void testHorizontalSymmetry() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#computeAngle(main.fr.ut2j.m1ice.ootesting.MyPoint)}.
+	 */
+	@Test
+	public void testComputeAngle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#rotatePoint(main.fr.ut2j.m1ice.ootesting.MyPoint, double)}.
+	 */
+	@Test
+	public void testRotatePoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#centralSymmetry(main.fr.ut2j.m1ice.ootesting.MyPoint)}.
+	 */
+	@Test
+	public void testCentralSymmetry() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#getMiddlePoint(main.fr.ut2j.m1ice.ootesting.MyPoint)}.
+	 */
+	@Test
+	public void testGetMiddlePoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#translate(double, double)}.
+	 */
+	@Test
+	public void testTranslateDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#setPoint(java.util.Random, java.util.Random)}.
+	 */
+	@Test
+	public void testSetPoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link main.fr.ut2j.m1ice.ootesting.MyPoint#translate(main.fr.ut2j.m1ice.ootesting.ITranslation)}.
+	 */
+	@Test
+	public void testTranslateITranslation() {
+		fail("Not yet implemented");
+	}
+
+}
